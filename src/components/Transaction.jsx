793a3b264c1e31e3951bdd398474402b42b3bb16
Saad Rahman <saadreh5@gmail.com>
2023-04-18 18:18:57 +0500

    starting working on tansaction component

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,3 +1,11 @@
+import '../styles/transaction.css'
 export default function Transaction(){
+
+    return <div className="transaction">
+        <h3>transaction</h3>
+        <div className="transaction-cards">
+            
+        </div>
+    </div>
     
 }
